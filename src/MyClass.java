2459e54d42b240ed5f4a9792da8a5de9f65f6de9
VesnaBogdanovic <antiopaquilling@gmail.com>
2024-12-02 22:53:54 +0100

    added new file

---
+++ src/MyClass.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class MyClass {
+    public static void main (String[] args){
+
+
+
+
+        int []array ={3,6,8,-1,10,11,5,34};
+        Arrays.sort(array);
+        for (int elements:array){
+            //System.out.println(" ");
+            System.out.println(" "  + elements);
+        }
+
+    }
+}
